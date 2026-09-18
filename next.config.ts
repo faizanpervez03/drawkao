7ecc981e6d72c0183040f46cfb5ac5ceb75f2d4c
faizanpervez03 <fk1335619@gmail.com>
2026-09-18 08:46:08 +0500

    Revert "Add hero section image, optimize image config"
    This reverts commit a20a0b49880a4a92aef0f5fd8beb7324957db0c1.

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   devIndicators: false,
-  images: {
-    formats: ["image/avif", "image/webp"],
-  },
 };
 
 export default nextConfig;
